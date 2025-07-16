bfe79711aa0f58b68984363f555a2544358d421f
CodesByPeter <petermusumba360@gmail.com>
2025-07-16 10:54:47 +0300

    Feature: Discarded its implementation since its logic was off and it was not also being used

--- internal/logics/inventory_logic.go
+++ internal/logics/inventory_logic.go
@@ -7,6 +7,5 @@ import (
 func Guide() {
 	// Welcome user and inquire about their next step
 	fmt.Println("Welcome to Dev_Inventory......... \n Are you currently: \n 1. Learning \n 2. Building a Project? ")
+	fmt.Println("Type 'EXIT' at any time to quit the program.")
 }
-
-
