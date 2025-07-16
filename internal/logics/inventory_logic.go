4c33d6646a4194d942ecb8f41cb2d5da644fe2fa
CodesByPeter <petermusumba360@gmail.com>
2025-07-16 10:45:53 +0300

    Feature: Eliminated the guide_choice function since it was being unused

--- internal/logics/inventory_logic.go
+++ internal/logics/inventory_logic.go
@@ -9,13 +9,4 @@ func Guide() {
 	fmt.Println("Welcome to Dev_Inventory......... \n Are you currently: \n 1. Learning \n 2. Building a Project? ")
 }
 
-func Guide_Choice(choice string) {
-	switch choice {
-	case "1":
-		fmt.Println("If you are learning are you learning: \n 1. A new thing you have never journalled. \n 2. A continous thing you have ever journalled before. ")
-	case "2":
-		fmt.Println("Are you engaging in: \n 1. A new project \n 2. A Continuing/Existing one ")
-	default:
-		fmt.Println("Not Building or Learning. Relax there ain't much of that in the job market.")
-	}
-}
+
